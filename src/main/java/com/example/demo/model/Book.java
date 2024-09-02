39ea7adcc46cdcdd9712ff36fddae784d1b103d1
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2024-09-02 22:43:19 +0900

    push

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    private Long book_id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @NotNull
+    @Column(nullable = false)
+    private String status;
+
+    // ゲッターとセッター
+    public Long getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(Long book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
